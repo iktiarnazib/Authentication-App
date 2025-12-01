b960d46492f044b805891f8cbeb07bfd22b3281e
Iktiar Nazib <iktiarnazib@gmail.com>
2025-12-01 14:42:08 +0600

    Auth app with all the functionality

---
+++ lib/views/pages/Delete_account.dart
@@ -0,0 +1,121 @@
+import 'package:authapp/app/mobile/auth_service.dart';
+import 'package:authapp/views/widget_tree.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class DeleteAccount extends StatefulWidget {
+  const DeleteAccount({super.key});
+
+  @override
+  State<DeleteAccount> createState() => _DeleteAccountState();
+}
+
+class _DeleteAccountState extends State<DeleteAccount> {
+  String errorMessage = '';
+  TextEditingController controllerEmail = TextEditingController();
+  TextEditingController controllerPass = TextEditingController();
+
+  void deleteAcc() async {
+    try {
+      await authService.value.deleteAccount(
+        email: controllerEmail.text,
+        password: controllerPass.text,
+      );
+      alert();
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message ?? 'There is an error';
+      });
+    }
+  }
+
+  void alert() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Your deletion'),
+          content: Text('Your account has been deleted successfully'),
+          actions: [
+            FilledButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return WidgetTree();
+                    },
+                  ),
+                );
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Delete account',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Lottie.asset('assets/lotties/TrashBin.json', height: 300.0),
+            SizedBox(height: 30.0),
+            Text(
+              'Delete your account',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            TextField(
+              controller: controllerEmail,
+              decoration: InputDecoration(
+                hintText: "Email",
+                hintStyle: TextStyle(color: Colors.white30),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              controller: controllerPass,
+              decoration: InputDecoration(
+                hintText: "Password",
+                hintStyle: TextStyle(color: Colors.white30),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+            ),
+            Text(errorMessage, style: TextStyle(color: Colors.red)),
+            FilledButton(
+              onPressed: () {
+                deleteAcc();
+              },
+              style: FilledButton.styleFrom(
+                minimumSize: Size(double.infinity, 40.0),
+              ),
+              child: Text("Change Password"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
